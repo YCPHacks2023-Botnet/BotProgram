fc794543e4f4840814d67cdc4958ca092f58ea26
Brandon Simmons <bsimmons1@ycp.edu>
2023-11-04 02:25:12 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# BotProgram
+
+#### Build program
+```shell
+g++ main.cpp -o worker -lstdc++
+```
+
+#### Run Local Server
+```
+json-server db.json --routes routes.json --host 0.0.0.0
+```
---
+++ worker.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+/*
+Worker class is defined to build out the bots object from json
+*/
+class Worker {
+public:
+    int id;
+    std::string ip;
+    std::string name;
+    std::string cpu;
+    std::string ram;
+
+    Worker() {
+        // Default constructor
+        // You can initialize the member variables to default values here
+        id = 0;
+        ip = "";
+        name = "";
+        cpu = "";
+        ram = "";
+    }
+
+    void setId(int _id) {
+        id = _id;
+    }
+
+    void setIp(const std::string& _ip) {
+        ip = _ip;
+    }
+
+    void setName(const std::string& _name) {
+        name = _name;
+    }
+
+    void setCpu(const std::string& _cpu) {
+        cpu = _cpu;
+    }
+
+    void setRam(const std::string& _ram) {
+        ram = _ram;
+    }
+};
+
